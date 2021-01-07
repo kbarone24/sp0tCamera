f3388d4dcb3000f407e518bbf13804656ec43d23
Kenny Barone <kennybarone@gmail.com>
2021-01-07 14:09:36 -0500

    Initial Commit

---
+++ sp0tCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sp0tCamera
+//
+//  Created by kbarone on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
